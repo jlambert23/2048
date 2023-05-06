c9c920911ba06bfc8194ea822810736783324a75
Justin Lambert <justin.lambert@synapsestudios.com>
2023-05-06 13:52:18 -0400

    add sample squares

--- src/core/colors-themes.ts
+++ src/core/colors-themes.ts
@@ -1,5 +1,24 @@
 import { MantineThemeColors } from "@mantine/core";
 
+export const SAMPLE_SQUARES = [
+  { x: 0, y: 0, value: 2 },
+  { x: 1, y: 0, value: 4 },
+  { x: 2, y: 0, value: 8 },
+  { x: 3, y: 0, value: 16 },
+  { x: 0, y: 1, value: 32 },
+  { x: 1, y: 1, value: 64 },
+  { x: 2, y: 1, value: 128 },
+  { x: 3, y: 1, value: 256 },
+  { x: 0, y: 2, value: 516 },
+  { x: 1, y: 2, value: 1024 },
+  { x: 2, y: 2, value: 2048 },
+  { x: 3, y: 2, value: 4096 },
+  { x: 0, y: 3, value: 8192 },
+  { x: 1, y: 3, value: 16384 },
+  { x: 2, y: 3, value: 32768 },
+  { x: 3, y: 3, value: 65536 },
+];
+
 export const colorThemes = (colors: MantineThemeColors) => ({
   rgb: {
     2: { backgroundColor: colors.red[9], color: colors.gray[3] },
